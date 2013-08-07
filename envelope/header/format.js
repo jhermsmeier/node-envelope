12a0890e4ecb02a7056317fd89db7a3709006a5a
jhermsmeier <jhermsmeier@googlemail.com>
2013-08-07 03:16:37 +0200

    Added envelope/header/format to reverse transforms
    Issue #10

---
+++ envelope/header/format.js
@@ -0,0 +1,102 @@
+/**
+ * Reverses the transform previously
+ * applied to the header field
+ * @param  {String} key
+ * @param  {Mixed}  value
+ * @return {String}  
+ */
+function format( key, value ) {
+  
+  for( var i in format.map ) {
+    if( ~format.map[i].indexOf( key ) )
+      return format.fn[i]( value )
+  }
+  
+  return value
+  
+}
+
+// Exports
+module.exports = format
+
+/**
+ * Maps header field names to
+ * their filter functions
+ * @type {Object}
+ */
+format.map = {
+  address:    [ 'from', 'replyTo', 'to', 'cc', 'bcc', 'sender', 'returnPath', 'deliveredTo' ],
+  content:    [ 'contentType', 'contentDisposition' ],
+  received:   [ 'received', 'xReceived' ],
+  references: [ 'references' ],
+  date:       [ 'date' ],
+}
+
+/**
+ * Transform functions
+ * @type {Object}
+ */
+format.fn = {
+  
+  date: function( input ) {
+    return new Date( input )
+      .toUTCString()
+  },
+  
+  received: function( input ) {
+    
+    var info = [ 'from', 'by', 'with', 'id', 'for' ]
+      .reduce( function( str, key ) {
+        
+        if( input[ key ] ) {
+          str = [ str, key, input[ key ] ].join( ' ' )
+          str = str.trim()
+        }
+        
+        return str
+        
+      }, '')
+    
+    var time = new Date( input.time )
+      .toUTCString()
+    
+    return input.time ?
+      info+'; '+time :
+      info
+    
+  },
+  
+  references: function( input ) {
+    return [].concat( input ).join( ' ' )
+  },
+  
+  address: function( input ) {
+    
+    if( input.name && input.address ) {
+      return input.name + ' <' + input.address + '>'
+    } else if( input.name ) {
+      return input.name
+    } else if( input.address ) {
+      return '<' + input.address + '>'
+    } else {
+      return ''
+    }
+    
+  },
+  
+  content: function( input ) {
+    
+    var fields = [ input.mime ]
+    
+    Object.keys( input ).forEach(
+      function( key ) {
+        if( key === 'mime' ) return
+        fields.push( key+'="'+input[key]+'"' )
+      }
+    )
+    
+    return fields.join( '; ' )
+    
+  }
+  
+}
