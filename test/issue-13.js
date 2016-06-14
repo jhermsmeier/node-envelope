a4e7a65ce70eee28d40fcf446478e83206cbf956
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-06-14 18:09:47 +0200

    Add test for Issue #13

---
+++ test/issue-13.js
@@ -0,0 +1,17 @@
+var assert = require( 'assert' )
+var tools = require( './tools' )
+var Envelope = require( '..' )
+
+suite( 'Envelope', function() {
+  
+  suite( 'Issue #13', function() {
+    
+    test( 'should not remove newlines or linefeeds from qp encoded body', function() {
+      var data = tools.read( 'issues/13' )
+      var mail = new Envelope( data )
+      assert.ok( mail['0']['0'].indexOf( '\r\n' ) >= 0 )
+    })
+    
+  })
+  
+})
--- test/tools/index.js
+++ test/tools/index.js
@@ -2,5 +2,5 @@ var tools = module.exports
 
 tools.read = function read( name ) {
   var fs = require( 'fs' )
-  return fs.readFileSync( __dirname + '/data/' + name + '.txt' )
+  return fs.readFileSync( __dirname + '/../data/' + name + '.txt' )
 }
