d2adef9c07f4724fc215d861a4428d9518993053
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-05-30 11:29:10 +0200

    chore(package): Add benchmarks

---
+++ benchmark/emails.js
@@ -0,0 +1,29 @@
+var bench = require( 'nanobench' )
+var fs = require( 'fs' )
+var path = require( 'path' )
+var Envelope = require( '..' )
+
+var ITERATIONS = 1000
+
+var dirname = path.join( __dirname, '..', 'test', 'data' )
+
+var emails = fs.readdirSync( dirname )
+  .filter( function( filename ) {
+    return /\.txt$/.test( filename )
+  }).map( function( filename ) {
+    return path.join( dirname, filename )
+  })
+
+emails.forEach( function( filename ) {
+
+  bench( `${path.basename( filename )} ⨉ ${ITERATIONS}`, function( run ) {
+    var buffer = fs.readFileSync( filename )
+    var email = null
+    run.start()
+    for( var i = 0; i < ITERATIONS; i++ ) {
+      email = new Envelope( buffer )
+    }
+    run.end()
+  })
+
+})
