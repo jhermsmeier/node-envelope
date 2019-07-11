b2b35baf26e360cb001ceee1d4e93c9bc1a35310
Jonas Hermsmeier <jhermsmeier@gmail.com>
2019-07-11 18:58:48 +0200

    test: Add tests for issues #22, and #23

--- test/issue-13.js
+++ test/issue-13.js
@@ -16,4 +16,26 @@ describe( 'Issues', function() {
 
   })
 
+  context( '#22', function() {
+
+    specify( 'should parse the "Subject" header correctly', function() {
+      var header = `Subject: =?utf-8?B?QVc6IEdyb8OfZSBHZWJ1cnRzdGFncy1QYXJ0eSBpbSBN?=\r\n\t=?utf-8?B?YWkgKGljaCBmZWllcmUgbmFjaHRyw6RnbGljaCkh?=\r\n\r\n`
+      var mail = new Envelope( header )
+      assert.strictEqual( mail.header.get( 'subject' ), 'AW: Große Geburtstags-Party im Mai (ich feiere nachträglich)!' )
+    })
+
+  })
+
+  context( '#23', function() {
+
+    specify( 'should parse the "From" header correctly', function() {
+      var header = 'From: "Almaifd, Tim" <T.Almaifd@redacted.com>\r\n\r\n'
+      var mail = new Envelope( header )
+      assert.deepEqual( mail.header.get( 'from' ), [
+        { name: 'Almaifd, Tim', address: 'T.Almaifd@redacted.com' },
+      ])
+    })
+
+  })
+
 })
