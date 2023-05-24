a4fd04d2067f9203241274963d17faff28334848
Brett Zamir <brettz9@yahoo.com>
2023-05-24 09:54:27 -0700

    feat: TS

---
+++ dist/envelope.d.ts
@@ -0,0 +1,60 @@
+export = Envelope;
+/**
+ * @extends {Array}
+ */
+declare class Envelope extends Array<any> {
+    /**
+     * Parse an email
+     * @param {Buffer|string} buffer
+     * @param {Number} [offset=0]
+     * @param {Number} [length]
+     * @returns {Envelope}
+     */
+    static parse(buffer: Buffer | string, offset?: number | undefined, length?: number | undefined): Envelope;
+    /**
+     * Create a new Envelope
+     * @param {Buffer|string} [value]
+     */
+    constructor(value?: string | Buffer | undefined);
+    header: import("./header");
+    body: string | Buffer | null;
+    /**
+     * Whether it is a multipart message
+     * @return {boolean}
+     */
+    get isMultipart(): boolean;
+    /**
+     * @param {string} type
+     * @param {boolean} [withDisposition=true]
+     */
+    getContent(type: string, withDisposition?: boolean | undefined): any;
+    /**
+     * Get the plaintext part of a message
+     * @returns {string}
+     */
+    getText(): string;
+    /**
+     * Get the HTML part of a message
+     * @returns {string}
+     */
+    getHTML(): string;
+    /**
+     * Get all attachments contained in the message
+     * @param {boolean} [withInline=true] - Whether to include inline attachments
+     * @returns {Array<Envelope>}
+     */
+    getAttachments(withInline?: boolean | undefined): Array<Envelope>;
+    /**
+     * Parse an email
+     * @param {Buffer|string} buffer
+     * @param {Number} [offset=0]
+     * @param {Number} [length]
+     * @returns {Envelope}
+     */
+    parse(buffer: Buffer | string, offset?: number | undefined, length?: number | undefined): Envelope;
+}
+declare namespace Envelope {
+    const Header_1: typeof import("./header");
+    export { Header_1 as Header };
+}
+//# sourceMappingURL=envelope.d.ts.map
--- test/inspect.js
+++ test/inspect.js
@@ -1,5 +1,9 @@
 var util = require( 'util' )
 
+/**
+ * @param {any} value
+ * @returns {string}
+ */
 function inspect( value ) {
   return util.inspect( value, inspect.options )
 }
@@ -9,6 +13,10 @@ inspect.options = {
   colors: process.stdout.isTTY,
 }
 
+/**
+ * @param {any} value
+ * @returns {void}
+ */
 inspect.log = function( value ) {
   process.stdout.write( inspect( value ) )
   process.stdout.write( '\n' )
